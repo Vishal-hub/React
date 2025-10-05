802078b01e753f4950f8215d4651cabdff181ae3
Vishal-hub <gaikwadvishal2001@gmail.com>
2025-10-05 07:04:50 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,64 @@
+
+{/* <h1 id="heading">React</h1> */}
+
+// const heading = React.createElement("h1",{id:"heading"},"React");
+// const root= ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+
+{/*
+    <div id="parent">
+    <div id="child">
+        <h1 id="heading">I am h1 tag</h1>
+    </div>
+    <div id="child2">
+        <h1 id="heading2">I am child2 h1 tag</h1>
+        <h2 id="heading2">I am child2 h2 tag</h2>
+    </div>
+</div> 
+*/}
+
+// ReactElements ARE (Objects) => HTML(Browser Understands) 
+// React elements are plain javascript objects which are used to create and display content on the screen
+// React elements are immutable (cannot be changed) once they are created
+// React elements are lightweight and faster than DOM elements
+
+// React.createElement() method can take only 3 arguments such as 
+// element type (eg. <div>), 
+// attributes to our tags (eg. <id="parent">)
+// and the content inside the tag (eg. "I am h1 tag")
+// but if we want to pass multiple children to parent then we have to pass array of children
+// here we pass array of children to parent div, to form above structure
+    
+const parent =React.createElement(
+    "div",
+    {id:"parent"},
+    [
+        React.createElement("div",{id:"child"},React.createElement(
+            "h1",
+            {id:"heading"},
+            "I am h1 tag"
+            )
+        ),
+        React.createElement(
+            "div",
+            {id:"child2"},
+            [
+                React.createElement(
+                "h1",
+                {id:"heading2"},
+                "I am child2 h1 tag"
+                ),
+                React.createElement(
+                "h1",
+                {id:"heading2"},
+                "I am child2 h2 tag"
+                )
+            ]
+        )
+    ]
+);
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
